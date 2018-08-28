c3a7963cd3500be09d0db75ecbcdeec288e0b89d
Igor Vedeneev <i.vedeneev@agima.ru>
2018-08-28 08:39:29 +0300

    folder structure #1

--- CollectionKit/CollectionCell.swift
+++ CollectionKit/CollectionCell.swift
@@ -27,7 +27,7 @@ class CollectionCell: UICollectionViewCell {
         textLabel.autoresizingMask = [.flexibleWidth]
         addSubview(textLabel)
     }
-    
+        
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
